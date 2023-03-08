56fbb2e147f3b8ee9b71aafae97852eb2a24fb66
RomanKucheriavyi <94791620+RomanKucheriavyi@users.noreply.github.com>
2023-03-08 16:09:58 +0200

    Add files via upload

--- react-modul/src/context/GameContext.jsx
+++ react-modul/src/context/GameContext.jsx
@@ -1,14 +1,22 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 const GameContext = createContext();
 
-export function GameProvider ({ children }) {
+export function GameProvider({ children }) {
     const [coinFlipResult, setCoinFlipResult] = useState([]);
     const [doorResult, setDoorResult] = useState([]);
     const [numResult, setNumResult] = useState([]);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        let totalNum = numResult.reduce((acc, item) => acc + +Object.values(item), 0);
+        let totalDoor = doorResult.reduce((acc, item) => acc + +Object.values(item), 0);
+        let totalCoin = coinFlipResult.reduce((acc, item) => acc + +Object.values(item), 0);
+        setResults([{ "Вгадай число": totalNum.toFixed(1) }, {"Вгадай двері":totalDoor.toFixed(1)}, {"Монетка": totalCoin.toFixed(1)}])
+    }, [numResult, doorResult, coinFlipResult]);
+
     return (
-        <GameContext.Provider 
+        <GameContext.Provider
             value={{
                 coinFlipResult,
                 doorResult,
@@ -20,7 +28,7 @@ export function GameProvider ({ children }) {
                 setResults
             }}
         >
-            { children }
+            {children}
         </GameContext.Provider>
     )
 };
